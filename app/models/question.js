0eaaa194c1e32c7e9e9282f463d251df71b797ab
Ben Petrosky <benpetrosky@hotmail.com>
2017-06-02 14:01:37 -0700

    answers to questions will now be added on that questions question page. fixed loop issue by calling model.questions in the loop on the index

--- app/models/question.js
+++ app/models/question.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   author: DS.attr(),
   inquiry: DS.attr(),
   thoughts: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -6,6 +6,14 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      newAnswer.save().then(function() {
+        return question.save();
+      })
+      this.transitionTo('question', question);
+    },
   update(question, params) {
 
     Object.keys(params).forEach(function(key) {
